4f3ba9e04c793e4b62cf06bfd43abd2b02a70394
Austin Hines <Austinhines0605@gmail.com>
2021-04-21 09:12:04 -0500

    Almost finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import MCUShows from './mcu-shows/MCUShows'
 
-function App() {
+const releaseDates = {
+  wandaVision: 'Jan 2021',
+  falconWinter: 'Mar 2021',
+  loki: 'June 2021',
+  hawkeye: 'Fall of 2021'
+ }
+
+ const dates = releaseDates
+
+ 
+
+
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Final React EXERCISE</h1>
+      <MCUShows startingItems={dates}/>
+      
     </div>
   );
 }
